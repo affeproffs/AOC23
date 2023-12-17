648cc84cafd3d7f4af7bfbc52df54377382b96c1
affeproffs <astenbor@hotmail.com>
2023-12-17 20:34:04 +0100

    cleanup day 15

--- 15/a.py
+++ 15/a.py
@@ -10,9 +10,8 @@ def HASH(s):
     return result
 
 def addLense(s: str):
-    dash = s.find("-")
-    operation = "=" if dash == -1 else "-"    
-    if operation == "-":
+    dash = s.find("-")    
+    if dash != -1:
         label = s[:dash]
         box = HASH(label)
         if label in boxes[box]:
@@ -22,19 +21,22 @@ def addLense(s: str):
         box = HASH(label)
         boxes[box][label] = int(focal)
 
-def score(boxes):
+def score(boxes):    
     total = 0
     for i, box in enumerate(boxes):
         for j, value in enumerate(box.values()):            
             total += (i + 1) * (j + 1) * value
     return total
 
-
 hashes = []
 for i, line in enumerate(stdin):
     hashes.extend(line.rstrip().split(","))
 
 boxes = [{} for _ in range(256)] # [box][label] => value
+ans1 = 0
 for h in hashes:
     addLense(h)
-print(score(boxes))
+    ans1 += HASH(h)
+ans2 = score(boxes)   
+
+print(ans1, ans2)
